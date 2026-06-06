20be2defaa3fcf7b7c6c23497c2f2cea21314492
Pranjal Singh <pranjalsingh10061997@gmail.com>
2026-06-06 10:45:27 +0530

    Bed, Table, Chair, TV, Wardrobe

---
+++ lib/widgets/common/item_editor_header.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ItemEditorHeader extends StatelessWidget {
+  const ItemEditorHeader({
+    super.key,
+    required this.title,
+    required this.editingEnabled,
+    required this.onToggleEdit,
+    required this.onDelete,
+    this.icon,
+  });
+
+  final String title;
+  final IconData? icon;
+  final bool editingEnabled;
+  final VoidCallback onToggleEdit;
+  final VoidCallback onDelete;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        if (icon != null) ...[
+          Icon(icon),
+          const SizedBox(width: 8),
+        ],
+        Expanded(
+          child: Text(title, style: const TextStyle(fontWeight: FontWeight.w600)),
+        ),
+        IconButton(
+          icon: Icon(editingEnabled ? Icons.edit_off_outlined : Icons.edit_outlined),
+          tooltip: editingEnabled ? 'Lock parameters' : 'Edit parameters',
+          color: editingEnabled ? Colors.orange.shade800 : null,
+          onPressed: onToggleEdit,
+        ),
+        IconButton(
+          icon: const Icon(Icons.delete_outline, color: Colors.red),
+          tooltip: 'Delete',
+          onPressed: onDelete,
+        ),
+      ],
+    );
+  }
+}
