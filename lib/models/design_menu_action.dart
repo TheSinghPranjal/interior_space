4293602ed78232b7e1b0e68a56ed4fdd34a3adcf
Pranjal Singh <pranjalsingh10061997@gmail.com>
2026-06-11 15:44:30 +0530

    window and ac changes

--- lib/models/design_menu_action.dart
+++ lib/models/design_menu_action.dart
@@ -20,7 +20,7 @@ extension DesignMenuActionInfo on DesignMenuAction {
         DesignMenuAction.flooring => 'Flooring',
         DesignMenuAction.ceiling => 'Ceiling',
         DesignMenuAction.doors => 'Doors',
-        DesignMenuAction.windows => 'Windows',
+        DesignMenuAction.windows => 'Windows & AC',
         DesignMenuAction.cupboards => 'Cupboards',
         DesignMenuAction.furniture => 'Furniture',
         DesignMenuAction.lighting => 'Lighting',
