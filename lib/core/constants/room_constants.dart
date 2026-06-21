9644367f306c788259d87c09e7debe8a52291294
Pranjal Singh <pranjalsingh10061997@gmail.com>
2026-06-21 18:28:21 +0530

    room constants

--- lib/core/constants/room_constants.dart
+++ lib/core/constants/room_constants.dart
@@ -3,8 +3,8 @@ class RoomConstants {
   static const double defaultLength = 8;
   static const double defaultHeight = 10;
 
-  static const double minWidth = 6;
-  static const double minLength = 6;
+  static const double minWidth = 1;
+  static const double minLength = 1;
   static const double minHeight = 7;
 
   static const double maxWidth = 50;
@@ -13,6 +13,8 @@ class RoomConstants {
 
   static const double defaultDoorWidth = 3;
   static const double defaultDoorHeight = 7;
+  static const double minDoorWidth = 2;
+  static const double maxDoorWidth = 20;
 
   static const double defaultTileLength = 2.5;
   static const double defaultTileWidth = 2.5;
