42199491a2b901d90a0a0a38f3d3275ba77cbe39
Pranjal Singh <pranjalsingh10061997@gmail.com>
2026-06-22 18:42:56 +0530

    fix : apartment fix dimension control for apartment

--- lib/screens/preview_3d_screen.dart
+++ lib/screens/preview_3d_screen.dart
@@ -70,7 +70,7 @@ class _Preview3DScreenState extends ConsumerState<Preview3DScreen> {
         child: SafeArea(
           child: Text(
             isApartment
-                ? '${project.apartmentLayout.placements.length} rooms • Pinch to zoom • Swipe to rotate'
+                ? '${project.apartmentLayout.name} • ${project.apartmentLayout.placements.length} rooms • Pinch to zoom • Swipe to rotate'
                 : 'Pinch to zoom • Swipe to rotate • Use arrows to walk',
             style: TextStyle(color: Colors.grey.shade400, fontSize: 12),
             textAlign: TextAlign.center,
