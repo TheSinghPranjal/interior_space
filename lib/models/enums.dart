b1e170c07b5214fabd45ed53847823c9b96d7951
Pranjal Singh <pranjalsingh10061997@gmail.com>
2026-06-29 14:03:29 +0530

    fix : polygon room fixes

--- lib/models/enums.dart
+++ lib/models/enums.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 enum WallId { front, back, left, right }
 
+/// Rectangular (width × length) or user-drawn polygon on the custom room grid.
+enum RoomShapeMode { rectangular, polygon }
+
 /// Which segment of a wall remains visible when [WallConfig.visibleFraction] &lt; 1.
 enum WallVisibleAlign { start, center, end }
 
