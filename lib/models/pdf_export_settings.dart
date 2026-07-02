5fd4340d1d2e0e9e0be3e207b402659b100c27a9
Pranjal Singh <pranjalsingh10061997@gmail.com>
2026-07-02 15:09:39 +0530

    fix : include sketch page

--- lib/models/pdf_export_settings.dart
+++ lib/models/pdf_export_settings.dart
@@ -3,11 +3,13 @@ class PdfExportSettings {
     this.include3dPreview = true,
     this.includeFrontView = true,
     this.includeTopView = true,
+    this.includeSketchInPdf = false,
   });
 
   final bool include3dPreview;
   final bool includeFrontView;
   final bool includeTopView;
+  final bool includeSketchInPdf;
 
   bool get shouldCapture3d =>
       include3dPreview && (includeFrontView || includeTopView);
@@ -16,11 +18,13 @@ class PdfExportSettings {
     bool? include3dPreview,
     bool? includeFrontView,
     bool? includeTopView,
+    bool? includeSketchInPdf,
   }) {
     return PdfExportSettings(
       include3dPreview: include3dPreview ?? this.include3dPreview,
       includeFrontView: includeFrontView ?? this.includeFrontView,
       includeTopView: includeTopView ?? this.includeTopView,
+      includeSketchInPdf: includeSketchInPdf ?? this.includeSketchInPdf,
     );
   }
 
@@ -28,6 +32,7 @@ class PdfExportSettings {
         'include3dPreview': include3dPreview,
         'includeFrontView': includeFrontView,
         'includeTopView': includeTopView,
+        'includeSketchInPdf': includeSketchInPdf,
       };
 
   factory PdfExportSettings.fromJson(Map<String, dynamic>? json) {
@@ -36,6 +41,7 @@ class PdfExportSettings {
       include3dPreview: json['include3dPreview'] as bool? ?? true,
       includeFrontView: json['includeFrontView'] as bool? ?? true,
       includeTopView: json['includeTopView'] as bool? ?? true,
+      includeSketchInPdf: json['includeSketchInPdf'] as bool? ?? false,
     );
   }
 }
