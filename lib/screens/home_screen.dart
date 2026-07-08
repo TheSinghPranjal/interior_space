c3084c0cf957e212748496c65a83d2e3fa217d1b
Pranjal Singh <pranjalsingh10061997@gmail.com>
2026-07-08 23:21:54 +0530

    fix : fix

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -401,6 +401,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                                 ],
                               ),
                             ),
+
                             const PopupMenuDivider(),
                             if (!isApartment) ...[
                               PopupMenuItem(
